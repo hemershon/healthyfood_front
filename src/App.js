c361f7d90ef3da30cf6c480689fd2d83f8bcaf13
Hemershon Silva <hemershon@gmail.com>
2022-10-09 20:05:06 -0300

    Feature/dependences (#1)
    * feat(build) install dependences for react
    
    * feat(build) install dependences for react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import "./App.scss";
+import { Container, Section } from 'rbx';
+import Header from "./components/header";
+import Home from "./screens/home";
 
-const App = () => <p>Healthy food</p>
-
+ 
+const App = () => (
+  <Fragment>
+    <Header />
+    <Section>
+      <Container>
+        <Home/>
+      </Container>
+    </Section>
+  </Fragment>
+)
+ 
 export default App;
